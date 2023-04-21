31dc0c34b3e5167c88e6db251a3e7cef80b796fd
dondon <domi61503@gmail.com>
2023-04-21 14:31:11 +0800

    feat:註冊頁改VUE 加上驗證功能

--- login.js
+++ login.js
@@ -6,14 +6,17 @@ Vue.createApp({
       nickname: "",
       email: "",
       password: "",
-      checkemail: "",
-      checkpassword: "",
+
       emptyEmailError: "",
       emptyPasswordError: "",
     };
   },
 
   methods: {
+    clear() {
+      this.email = "";
+      this.password = "";
+    },
     //驗證欄位空值
     vertify() {
       if (this.email.trim() == "") {
@@ -32,27 +35,32 @@ Vue.createApp({
           password: this.password,
         },
       };
-      axios
-        .post("https://todoo.5xcamp.us/users/sign_in", obj)
-        .then((response) => {
-          console.log(response.data);
-          if (this.password.length < 5) {
-            Swal.fire("登入失敗", "密碼不得少於六位數", "warning");
-          }
-          if (response.data.message == "登入成功") {
-            this.token = response.headers.authorization;
-            this.nickname = response.data.nickname;
-            localStorage.setItem("authorization", this.token);
-            localStorage.setItem("nickname", this.nickname);
-            location.href = "todo.html";
-          }
-        })
-        .catch((error) => {
-          console.log(error);
-          if (error.message == "Request failed with status code 401") {
-            Swal.fire("登入失敗", "請先註冊帳號", "error");
-          }
-        });
+      if (this.email === "" || this.password === "") {
+        return;
+      } else if (this.password.length < 5) {
+        Swal.fire("登入失敗", "密碼不得少於六位數", "warning");
+        this.clear();
+      } else {
+        axios
+          .post("https://todoo.5xcamp.us/users/sign_in", obj)
+          .then((response) => {
+            console.log(response.data);
+            if (response.data.message == "登入成功") {
+              this.token = response.headers.authorization;
+              this.nickname = response.data.nickname;
+              localStorage.setItem("authorization", this.token);
+              localStorage.setItem("nickname", this.nickname);
+              location.href = "todo.html";
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+            if (error.message == "Request failed with status code 401") {
+              Swal.fire("登入失敗", "請先註冊帳號", "error");
+              this.clear();
+            }
+          });
+      }
     },
     // onPressEnter() {
     //   this.logIn();
