8ded1c7f2a7b33bd700e0748af6d9e4ebf0a76a1
dondon <domi61503@gmail.com>
2023-04-23 13:24:00 +0800

    BUG版本

--- register.js
+++ register.js
@@ -1,82 +1,3 @@
-// const email = document.querySelector("#email");
-// const p_account = document.querySelector(".alert-email");
-
-// const nickname = document.querySelector("#name");
-// const p_nickname = document.querySelector(".alert-name");
-
-// const pwd = document.querySelector("#pwd");
-// const p_pwd = document.querySelector(".alert-pwd");
-
-// const pwd_check = document.querySelector("#pwd_check");
-// const p_pwd_check = document.querySelector(".alert-pwdcheck");
-
-// const register = document.querySelector(".btn-register");
-
-// const register_form = document.querySelector(".form");
-
-// const input = document.querySelector(".input");
-// // 驗證欄位是否空值
-// let register_verify = (e) => {
-//   if (email.value.trim() == "") {
-//     p_account.innerHTML =
-//       "<div class='text-red-500 font-bold'>帳號尚未填寫</div>";
-//   }
-//   if (nickname.value.trim() == "") {
-//     p_nickname.innerHTML =
-//       "<div class='text-red-500 font-bold'>暱稱尚未填寫</div>";
-//   }
-//   if (pwd.value.trim() == "") {
-//     p_pwd.innerHTML = "<div class='text-red-500 font-bold'>密碼尚未填寫</div>";
-//   }
-//   if (pwd_check.value.trim() == "") {
-//     p_pwd_check.innerHTML =
-//       "<div class='text-red-500 font-bold'>確認密碼尚未填寫</div>";
-//   }
-// };
-
-// //註冊按鈕
-// register.addEventListener("click", () => {
-//   // 監聽驗證
-//   register_verify();
-//   callSignUp();
-// });
-
-// // let obj = {
-// //   user: {
-// //     email: "123@gmail.com",
-// //     nickname: "string",
-// //     password: "string",
-// //   },
-// // };
-// const callSignUp = () => {
-//   let obj = {
-//     user: {
-//       email: email.value,
-//       nickname: nickname.value,
-//       password: pwd.value,
-//     },
-//   };
-
-//   axios
-//     .post("https://todoo.5xcamp.us/users", obj)
-//     .then((response) => {
-//       if (response.data.message == "註冊成功") {
-//         alert("恭喜帳號註冊成功");
-//       } else if (response.data.message == "註冊發生錯誤") {
-//         alert("帳號註冊失敗，有可能有人用你的email註冊！");
-//       }
-//       register_form.reset();
-//       location.href = "index.html";
-//     })
-//     .catch((error) => {
-//       alert("此帳號密碼已存在，請重新註冊");
-//       signUpEmail.value = "";
-//       signUpNick.value = "";
-//       signUpPassword.value = "";
-//       signUpPasswordAgain.value = "";
-//     });
-// };
-
 //VUE
 Vue.createApp({
   data() {
@@ -140,10 +61,10 @@ Vue.createApp({
           .post("https://todoo.5xcamp.us/users", obj)
           .then((response) => {
             if (response.data.message == "註冊成功") {
-              alert("恭喜帳號註冊成功");
-              Swal.fire("註冊成功", "恭喜", "success");
+              Swal.fire("註冊成功", "恭喜", "success").then(() => {
+                location.href = "index.html";
+              });
             }
-            location.href = "index.html";
           })
           .catch((error) => {
             Swal.fire("註冊失敗", "此帳號已被使用", "warning");
--- todo.js
+++ todo.js
@@ -1,371 +1,232 @@
-const text = document.querySelector(".text");
-const save = document.querySelector(".save");
-const list = document.querySelector(".list");
-const user = document.querySelector(".user");
-const list_box = document.querySelector(".list-box");
-const empty = document.querySelector(".empty");
-const input = document.querySelector(".input");
-const logoutBtn = document.querySelector(".logoutBtn");
-const edit = document.querySelector(".edit");
-const checkBox = document.querySelector(".checkbox");
-const countDataDom = document.querySelector(".countdata");
+//VUE
+Vue.createApp({
+  data() {
+    return {
+      token: localStorage.getItem("authorization"),
+      nickname: localStorage.getItem("nickname"),
+      url: "https://todoo.5xcamp.us",
+      data: [],
+      textContent: "",
+      Done: [],
+      notDone: [],
+      showNotDoneMessage: true,
+      showDoneMessage: false,
+      showall: true,
+      showNotDone: false,
+      showDone: false,
+    };
+  },
 
-let current_tab = "allBtn"; // all, notDone, done
-let data = [];
-
-// const allBtn = document.querySelector(".allbtn");
-// const notDoneBtn = document.querySelector(".notdonebtn");
-// const doneBtn = document.querySelector(".donebtn");
-
-const _url = "https://todoo.5xcamp.us";
-const token = localStorage.getItem("authorization");
-const nickname = localStorage.getItem("nickname");
-console.log(token);
-
-// 判斷0筆跟有資料的時候切換背景圖
-
-const switch_statues = () => {
-  if (data.length == 0) {
-    list_box.classList.add("hidden");
-    empty.classList.remove("hidden");
-  } else if (data.length > 0) {
-    list_box.classList.remove("hidden");
-    empty.classList.add("hidden");
-    const cleanBtn = document.querySelector(".cleanbtn");
-    cleanBtn.addEventListener("click", () => {
-      cleanDoneTodo();
-    });
-    document.getElementById("allBtn").addEventListener("click", (e) => {
-      allBtn.classList.add(
-        "border-b-4",
-        "border-black",
-        "text-[20px]",
-        "font-black"
-      );
-      doneBtn.classList.remove(
-        "border-b-4",
-        "border-black",
-        "text-[20px]",
-        "font-black"
-      );
-      notDoneBtn.classList.remove(
-        "border-b-4",
-        "border-black",
-        "text-[20px]",
-        "font-black"
-      );
-      current_tab = "allBtn";
-      renderData();
-    });
-
-    document.getElementById("notDoneBtn").addEventListener("click", (e) => {
-      notDoneBtn.classList.add(
-        "border-b-4",
-        "border-black",
-        "text-[20px]",
-        "font-black"
-      );
-      allBtn.classList.remove(
-        "border-b-4",
-        "border-black",
-        "text-[20px]",
-        "font-black"
-      );
-      doneBtn.classList.remove(
-        "border-b-4",
-        "border-black",
-        "text-[20px]",
-        "font-black"
-      );
-      current_tab = "notDoneBtn";
-      renderData();
-    });
-
-    document.getElementById("doneBtn").addEventListener("click", (e) => {
-      doneBtn.classList.add(
-        "border-b-4",
-        "border-black",
-        "text-[20px]",
-        "font-black"
-      );
-      allBtn.classList.remove(
-        "border-b-4",
-        "border-black",
-        "text-[20px]",
-        "font-black"
-      );
-      notDoneBtn.classList.remove(
-        "border-b-4",
-        "border-black",
-        "text-[20px]",
-        "font-black"
-      );
-      current_tab = "doneBtn";
-      renderData();
-    });
-  }
-};
-
-// 取得 todo
-const getTodo = () => {
-  axios
-    .get(`${_url}/todos`, {
-      headers: {
-        authorization: token,
-      },
-    })
-    .then((response) => {
-      // console.log(response.data);
-      data = response.data.todos;
-      renderData(data);
-      switch_statues();
-      // console.log(data.length);
-    });
-};
-
-// 在一進入網頁時就呼叫 getTodo()
-axios
-  .get(`${_url}/check`, {
-    headers: {
-      authorization: token,
+  methods: {
+    test() {
+      console.log(this.token);
+      console.log(this.nickname);
+      console.log(this.url);
     },
-  })
-  .then(() => {
-    user.textContent = `${nickname}的待辦`;
-    getTodo();
-  })
-  .catch(() => {
-    location.href = "index.html";
-  });
-
-// 渲染TODO
-const renderData = () => {
-  const str = data
-    .filter(filterDisplayData)
-    .map((item, index) => {
-      // console.log(item.id);
-      return `
-        <ul class="flex items-center justify-between border-b-2 py-4">
-          <li class="flex">
-            <img class="checkbox flex h-[22px] w-[22px] cursor-pointer " type="checkbox" data-num="${
-              item.id
-            }" src="${
-        item.completed_at !== null
-          ? "./images/check.svg"
-          : "./images/checkbox.svg"
-      }" />
-            <p class="${
-              item.completed_at !== null
-                ? "ml-4 edit line-through"
-                : "ml-4 edit"
-            }">${item.content}</p>
-          </li>
-          <li>
-            <img src="images/Vector.svg" alt="" data-num="${
-              item.id
-            }" id="delete" class=" cursor-pointer"/>
-          </li>
-        </ul>
-
-      `;
-    })
-    .join("");
-  let count = "";
-  if (current_tab === "allBtn" || current_tab === "notDoneBtn") {
-    count = `<p>${notDoneLength()}個待完成事項</p>`;
-  } else {
-    let doneLength = data.filter(
-      (item) => item.completed_at !== null && item.completed_at !== undefined
-    ).length;
-    count = `<p>${doneLength}個已完成事項</p>`;
-  }
-
-  const countData_str = `          
-          <div class="flex justify-between mt-4">
-           ${count}
-            <p class="cursor-pointer text-[#9F9A91] cleanbtn" name="cleanbtn">清除已完成項目</p>
-          </div>`;
-  // 計算有幾筆待完成事項
-
-  list.innerHTML = str;
-  countDataDom.innerHTML = countData_str;
-};
-
-// 新增todo
-const addtodo = () => {
-  if (text.value.trim() == "") {
-    alert("請輸入內容");
-    return;
-  }
-  const obj = {};
-  obj.content = text.value;
-  // console.log(obj);
-  data.push(obj);
-  renderData();
-  switch_statues();
-
-  let addObj = {
-    todo: {
-      content: text.value,
+    click() {
+      2 > 1;
     },
-  };
+    // 沒有token就登出
+    tokentest() {
+      axios
+        .get(`${this.url}/check`, {
+          headers: {
+            authorization: this.token,
+          },
+        })
+        .then((res) => {
+          this.getTodo();
+          // console.log(res);
+        })
+        .catch(() => {
+          location.href = "index.html";
+        });
+    },
+    // 取得
+    getTodo() {
+      axios
+        .get(`${this.url}/todos`, {
+          headers: {
+            authorization: this.token,
+          },
+        })
+        .then((response) => {
+          // console.log(response.data);
+          this.data = response.data.todos;
+          this.count();
+        });
+    },
+    addItem() {
+      if (this.textContent.trim() === "") {
+        Swal.fire("新增失敗", "請輸入內容再新增", "warning");
+      } else {
+        let addObj = {
+          todo: {
+            content: this.textContent,
+          },
+        };
 
-  //輸入後表單清空
-  input.reset();
-
-  axios
-    .post(`${_url}/todos`, addObj, {
-      headers: {
-        authorization: token,
-      },
-    })
-    .then((response) => {
-      // console.log(response.data);
-      // console.log(response);
-      data.push(response.data);
-      getTodo();
-    })
-    .catch((error) => {
-      alert(error);
-    });
-};
-save.addEventListener("click", addtodo);
-
-// 刪除todo
-list.addEventListener("click", (e) => {
-  if (e.target.getAttribute("id") !== "delete") {
-    return;
-  }
-  // 拿到todo列表的id
-  const targetId = e.target.getAttribute("data-num");
-
-  // 過濾出新陣列，刪除索引為 targetId 的元素
-  data = data.filter((item, index) => index !== Number(targetId));
-
-  // 根據 targetId 找到要刪除的元素
-  const targetItem2 = data.find((item) => item.id === targetId);
-
-  // 使用 axios 發送刪除請求
-  axios
-    .delete(`${_url}/todos/${targetId}`, {
-      headers: {
-        authorization: token,
-      },
-    })
-    .then(() => {
-      // 從陣列中刪除目標元素
-      data.splice(data.indexOf(targetItem), 1);
-      // 重新渲染列表
-      renderData();
-
-      switch_statues();
-    });
-});
-
-// 刪除已完成todo
-const cleanDoneTodo = () => {
-  // 篩選需要被刪除的清單(已完成)
-  let cleanList = data.filter(
-    (item) => item.completed_at !== undefined && item.completed_at !== null
-  );
-  // console.log(data);
-
-  Promise.all(
-    cleanList.map((item) => {
-      return axios.delete(`${_url}/todos/${item.id}`, {
-        headers: {
-          authorization: token,
-        },
+        //輸入後表單清空
+        this.textContent = "";
+        // 戳新增API
+        axios
+          .post(`${this.url}/todos`, addObj, {
+            headers: {
+              authorization: this.token,
+            },
+          })
+          .then((response) => {
+            this.data.push(response.data);
+            this.count();
+            this.getTodo();
+            Swal.fire("成功", "新增一筆代辦事項", "success");
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    },
+    checkBoxToDone(targetId, index) {
+      axios
+        .patch(`${this.url}/todos/${targetId}/toggle`, "", {
+          headers: {
+            authorization: this.token,
+          },
+        })
+        .then((res) => {
+          this.data.splice(index, 1, res.data);
+          this.count();
+        });
+    },
+    deleteItem(targetId, index) {
+      Swal.fire({
+        title: "請確認",
+        text: "一但刪除將無法回復",
+        icon: "error",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "確定刪除",
+        cancelButtonText: "再想想",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire("已刪除", "您的代辦事項刪除成功", "success");
+          console.log(targetId);
+          axios
+            .delete(`${this.url}/todos/${targetId}`, {
+              headers: {
+                authorization: this.token,
+              },
+            })
+            .then((res) => {
+              this.data.splice(index, 1);
+              this.count();
+            });
+        } else {
+          return;
+        }
       });
-    })
-  ).then(() => {
-    data = data.filter(
-      (item) => item.completed_at === undefined || item.completed_at === null
-    );
-    renderData();
-  });
-};
+    },
+    cleanDone(targetId, index) {
+      Swal.fire({
+        title: "請確認是否刪除全部",
+        text: "一但刪除將無法回復",
+        icon: "error",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "確定刪除",
+        cancelButtonText: "再想想",
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire("已刪除", "您的所有已完成事項刪除成功", "success");
+          Promise.all(
+            this.Done.map((item) => {
+              return axios.delete(`${this.url}/todos/${item.id}`, {
+                headers: {
+                  authorization: this.token,
+                },
+              });
+            })
+          ).then(() => {
+            // this.data.splice(this.Done, this.Done.length);
+            this.data = this.data.filter((item) => {
+              return (
+                item.completed_at === null || item.completed_at === undefined
+              );
+            });
 
-// 登出功能
-const logout = () => {
-  axios
-    .delete(`${_url}/users/sign_out`, {
-      headers: {
-        authorization: token,
-      },
-    })
-    .then((response) => {
-      localStorage.removeItem("authorization");
-      localStorage.removeItem("nickname");
-      alert(response.data.message);
-      location.href = "index.html";
-    })
-    .catch((error) => {
-      console.log(error);
-      alert("登出失敗");
-    });
-};
-// 當登出被點擊
-logoutBtn.addEventListener("click", () => {
-  logout();
-});
+            this.count();
+          });
+        } else {
+          return;
+        }
+      });
+      // this.data.splice(this.Done, this.Done.length);
+      // this.count();
+    },
+    count() {
+      for (const item of this.data) {
+        if (item.completed_at === null || item.completed_at === undefined) {
+          this.notDone.push(item);
+        } else {
+          this.Done.push(item);
+        }
+      }
+    },
+    all() {
+      this.showDoneMessage = false;
+      this.showNotDoneMessage = true;
+      this.showall = true;
+      this.showDone = false;
+      this.showNotDone = false;
+    },
+    not() {
+      this.notDone = this.data.filter((item) => {
+        return item.completed_at === null || item.completed_at === undefined;
+      });
 
-// 切換狀態
-list.addEventListener("click", (e) => {
-  if (e.target.getAttribute("type") !== "checkbox") {
-    return;
-  }
-  // 拿到todo列表的id
-  const targetId = e.target.getAttribute("data-num");
-  let targetObj = data.filter((item) => item.id === targetId)[0]; // 找出被勾選的整筆資料
-  // data = data.filter((item, index) => index !== Number(targetId));
-  // renderData();
-  // switch_statues();
-  // console.log(e.target.parentNode);
+      this.showDoneMessage = false;
+      this.showNotDoneMessage = true;
+      this.showall = false;
+      this.showDone = false;
+      this.showNotDone = true;
+    },
+    done() {
+      this.Done = this.data.filter((item) => {
+        return item.completed_at !== null && item.completed_at !== undefined;
+      });
 
-  // // console.log(targetId);
-  // const targetItem = data.filter((item) => item.id === targetId)[0];
-  // console.log(targetItem);
+      this.showDoneMessage = true;
+      this.showNotDoneMessage = false;
+      this.showall = false;
+      this.showNotDone = false;
+      this.showDone = true;
+    },
 
-  axios
-    .patch(`${_url}/todos/${targetId}/toggle`, "", {
-      headers: {
-        authorization: token,
-      },
-    })
-    .then((res) => {
-      console.log("res", res);
-      data.splice(data.indexOf(targetObj), 1, res.data); //從data中把被勾選的資料拿掉，把新的資料放進去
-      console.log("data >>>", data);
-      renderData();
-      switch_statues();
-      // console.log(data);
-    });
-});
-
-// ENTER 新增todo
-const enter = (e) => {
-  if (e.keyCode !== 13) {
-    return;
-  }
-  addtodo();
-};
-
-const notDoneLength = () => {
-  return data.filter(
-    (item) => item.completed_at === null || item.completed_at === undefined
-  ).length;
-};
-
-const filterDisplayData = (item) => {
-  switch (current_tab) {
-    case "allBtn":
-      return true;
-    case "notDoneBtn":
-      return item.completed_at === null || item.completed_at === undefined;
-    case "doneBtn":
-      return !(item.completed_at === null || item.completed_at === undefined);
-  }
-};
-
-document.addEventListener("keydown", enter);
+    logOut() {
+      axios
+        .delete(`${this.url}/users/sign_out`, {
+          headers: {
+            authorization: this.token,
+          },
+        })
+        .then((response) => {
+          localStorage.removeItem("authorization");
+          localStorage.removeItem("nickname");
+          Swal.fire("登出成功", "歡迎下次使用", "success").then(() => {
+            location.href = "index.html";
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+          Swal.fire("登出失敗", "error");
+        });
+    },
+  },
+  mounted() {
+    this.tokentest();
+    this.count();
+    this.getTodo();
+  },
+}).mount("#app");
